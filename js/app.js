a1ad2683ce1564ca5e08b1157f74e952f25bd481
= <tsmit291@icloud.com>
2016-02-25 11:25:24 -0700

    page is loading with partials set up

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 // yo homie only your routes go in here //
 var app = angular.module('myApp', ['ngRoute']);
-app.controller("mainController", function($scope){
 
-
-
-
-
-
-
-
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/movies', {
+    templateUrl: './partials/home.html',
+    controller: 'mainController'
+  })
 });
